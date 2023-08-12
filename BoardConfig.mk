5687e3ea8bf035a7975d8a0200f3440828862c7b
Cyborg2017 <wangyang0987654@gmail.com>
2023-08-12 13:40:17 +0800

    nx651j_eea: Add nfc related definitions entirely
    Change-Id: Ifd920d68db4bedb7a60cb1fdc4d06e048127b5a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,17 @@ DEVICE_PATH := device/nubia/nx651j_eea
 # Assert
 TARGET_OTA_ASSERT_DEVICE := nx651j_eea,NX651J-EEA
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lineageos_nx651j-eea_defconfig
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/nubia/nx651j_eea/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -18,6 +18,30 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Inherit from sm7250-common
 $(call inherit-product, device/nubia/sm7250-common/lito.mk)
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service.samsung \
+    NfcNci \
+    Tag \
+    SecureElement
+
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.2.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.nfc.sku.sh
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
